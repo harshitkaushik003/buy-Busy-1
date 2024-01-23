0cb3586e83769c86c473d00195ae651077351210
harshitkaushik003 <harshitkaushik394@gmail.com>
2024-01-24 00:02:21 +0530

    initialised firebase

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDV2w8JX108aQDpA6APfr7TZVI5jtxTkRU",
+  authDomain: "buybusy-ba782.firebaseapp.com",
+  projectId: "buybusy-ba782",
+  storageBucket: "buybusy-ba782.appspot.com",
+  messagingSenderId: "560304489408",
+  appId: "1:560304489408:web:5f8860fe68eed8dbd5fe25"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export {db};
